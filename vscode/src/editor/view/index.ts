1b823cfce02460cde47511d6852d18f3c666f629
stanley <stanleygomesdasilva@gmail.com>
2026-05-31 21:14:49 -0300

    chore: remove vscode extension

--- vscode/src/editor/view/index.ts
+++
@@ -1,8 +0,0 @@
-export { openNote } from './openNote';
-export { createHandleCreate } from './handleCreate';
-export { createHandleDelete } from './handleDelete';
-export { createHandleRename } from './handleRename';
-export { createHandleDuplicate } from './handleDuplicate';
-export { createHandleChangeColor } from './handleChangeColor';
-export { createHandleImport } from './handleImport';
-export { createHandleCreateFromSelection } from './handleCreateFromSelection';
